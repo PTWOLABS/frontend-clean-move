34be8037aa53b75659f4c0fe4b91e4d60ffa79a0
Felipe Pereira <fehpereira237@gmail.com>
2026-05-28 18:27:36 -0300

    feat(appointments): enhance QUERY_KEYS for appointments with detailed key generation and add unit tests

--- src/shared/constants/query-keys.ts
+++ src/shared/constants/query-keys.ts
@@ -2,6 +2,7 @@ import type { AppointmentsFilters } from "@/features/appointments/types/api-filt
 import type { ListCustomersQuery } from "@/features/customer/types";
 import type { ListVehiclesQuery, VehicleOptionsQuery } from "@/features/vehicle/types";
 import type { OptionsQuery } from "@/shared/types/options-query";
+import { AppointmentsQueryKeyParams } from "../types/appointments";
 
 export const QUERY_KEYS = {
   authSession: ["auth", "session"] as const,
@@ -13,12 +14,29 @@ export const QUERY_KEYS = {
   metricsAppointment: ["metrics-appointment"],
   popularServices: ["popular-services"],
   revenueAndAppointments: ["revenue-and-appointments"],
-  appointments: (filters?: AppointmentsFilters, appointmentId?: string) =>
-    [
-      "appointments",
-      ...(filters ? [filters] : []),
-      ...(appointmentId ? [appointmentId] : []),
-    ] as const,
+  appointments: (params?: AppointmentsQueryKeyParams) => {
+    const root = ["appointments"] as const;
+
+    if (!params) {
+      return root;
+    }
+
+    const { appointmentId, filters } = params;
+
+    if (appointmentId && filters) {
+      return [...root, "detail", appointmentId, "filters", filters] as const;
+    }
+
+    if (appointmentId) {
+      return [...root, "detail", appointmentId] as const;
+    }
+
+    if (filters) {
+      return [...root, "list", filters] as const;
+    }
+
+    return root;
+  },
   services: (filters?: { page?: number; size?: number; name?: string; isActive?: boolean }) =>
     filters ? (["services", filters] as const) : (["services"] as const),
   customers: (filters?: ListCustomersQuery) =>
