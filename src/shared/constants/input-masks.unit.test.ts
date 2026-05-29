f65eefe3b5bb2107cda01f0b2dcdc3f85b9920fc
Pedrowill21 <pedrowilliam2999@gmail.com>
2026-05-29 11:29:18 -0300

    feat(input-masks): add constants for phone, CPF, CNPJ, and ZIP code masks with corresponding unit tests

---
+++ src/shared/constants/input-masks.unit.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "vitest";
+
+import {
+  CNPJ_MASK,
+  CPF_MASK,
+  getCpfCnpjMask,
+  PHONE_MASK,
+  ZIP_CODE_MASK,
+} from "./input-masks";
+
+describe("input-masks", () => {
+  it("should expose stable mask constants", () => {
+    expect(PHONE_MASK).toBe("(__) _____-____");
+    expect(CPF_MASK).toBe("___.___.___-__");
+    expect(CNPJ_MASK).toBe("__.___.___/____-__");
+    expect(ZIP_CODE_MASK).toBe("_____-___");
+  });
+
+  it("should use CPF mask for up to 11 digits", () => {
+    expect(getCpfCnpjMask("")).toBe(CPF_MASK);
+    expect(getCpfCnpjMask("123.456.789-0")).toBe(CPF_MASK);
+    expect(getCpfCnpjMask("12345678901")).toBe(CPF_MASK);
+  });
+
+  it("should switch to CNPJ mask after 11 digits", () => {
+    expect(getCpfCnpjMask("123456789012")).toBe(CNPJ_MASK);
+    expect(getCpfCnpjMask("12.345.678/0001-90")).toBe(CNPJ_MASK);
+  });
+});
