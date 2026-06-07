bcb049a42e18c26c22df7af6653cf28016b3062f
Pedrowill21 <pedrowilliam2999@gmail.com>
2026-06-07 16:55:58 -0300

    feat: add establishment domain types

---
+++ src/features/establishment/types/index.ts
@@ -0,0 +1,19 @@
+export type Establishment = {
+  id: string;
+  tradeName: string | null;
+  legalBusinessName: string | null;
+  cnpj: string | null;
+  slug: string | null;
+};
+
+export type GetEstablishmentResponse = {
+  establishment: Establishment;
+};
+
+export type UpdateEstablishmentPayload = Partial<
+  Pick<Establishment, "tradeName" | "legalBusinessName" | "cnpj" | "slug">
+>;
+
+export type UpdateEstablishmentResponse = {
+  establishment: Establishment;
+};
