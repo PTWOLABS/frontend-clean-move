c68d3998691ac0a12ad92d464dc6ed66e29b2bbc
FehPereira <fehpereira237@gmail.com>
2026-06-10 12:43:52 -0300

    feat: increase progressBar height

--- src/shared/providers/bprogress-provider.tsx
+++ src/shared/providers/bprogress-provider.tsx
@@ -4,5 +4,9 @@ import { AppProgressProvider } from "@bprogress/next";
 import { ReactNode } from "react";
 
 export function BProgressProvider({ children }: { children: ReactNode }) {
-  return <AppProgressProvider options={{ showSpinner: false }}>{children}</AppProgressProvider>;
+  return (
+    <AppProgressProvider options={{ showSpinner: false }} height="5px">
+      {children}
+    </AppProgressProvider>
+  );
 }
